5e592fba3f43c9ab5c3312e33b0211ddc12dde57
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2017-06-03 20:11:35 +0100

    continuing with product

--- dist/bundle.js
+++ dist/bundle.js
@@ -13113,6 +13113,8 @@ function Product(graph1, graph2){
     let graph = new Graph();
     this.resultGraph = graph;
     this.chooseNodes();
+    this.addTransitions(this.graph1);
+    this.addTransitions(this.graph2);
     console.log(graph);
 }
 
@@ -13126,7 +13128,7 @@ Product.prototype.chooseNodes = function(){
 
         for(var j = 0; j < this.graph2.nodeSet.length; j++){
             let node2 = this.graph2.nodeSet[j];
-            let newVal = name + node2.val;
+            let newVal = [name, node2.val];
             
             let newNode;
             if(node1.acceptanceNode || node2.acceptanceNode)
@@ -13140,6 +13142,50 @@ Product.prototype.chooseNodes = function(){
     
          }
     }
+}
+
+Product.prototype.addTransitions = function(graph){
+
+    console.log(graph);
+
+    for(var i = 0; i < graph.nodeSet.length; i++){
+        let node = graph.nodeSet[i];
+        
+        for(var j=0; j< node.edgeSet.length; j++){
+            var edge = node.edgeSet[j];
+            var transition = edge.transition;
+
+            console.log(edge);
+
+            var arrayNodes = this.getRelatedNodes(node);
+            console.log("Array originial");
+            console.log(arrayNodes);
+            console.log("Array Destino");
+            var destinationNodes = this.getRelatedNodes(edge.nodeTo);
+            console.log(destinationNodes);
+
+             for(var k=0; k< arrayNodes.length; k++){
+                for(var l=0; k < destinationNodes.length; k++){
+                    if(!destinationNodes[l].nodeEquals(destinationNodes[l],arrayNodes[k])){
+                        let edge = new Edge(destinationNodes[l],transition);
+                        arrayNodes[k].addEdge(edge);
+                    }
+                }      
+            }
+        }
+    }
+}
+
+Product.prototype.getRelatedNodes = function(node){
+    let array = new Array();
+
+    for(var i = 0; i < this.resultGraph.nodeSet.length; i++){
+        let nodeR = this.resultGraph.nodeSet[i].val;
+        if(nodeR.includes(node.val))
+            array.push(this.resultGraph.nodeSet[i]);
+    }
+
+    return array;
 }
 
 exports.Product = Product;
